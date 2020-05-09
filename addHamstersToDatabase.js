3a189be58f6ced78e577e9fe55885ec066062538
Alexander Rusiecki <alexander.rusiecki@gmail.com>
2020-05-09 05:12:52 +0200

    deleted script.js, added function to add collection to db, added folders .gitignore

---
+++ addHamstersToDatabase.js
@@ -0,0 +1,16 @@
+const { db } = require("./firebase");
+const hamsters = require("./data.json");
+
+// Selv-invoking at server start
+const addHamstersToDatabase = (() => {
+  try {
+    hamsters.forEach((hamster) => {
+      db.collection("hamsters").doc(hamster.id.toString()).set(hamster);
+      console.log(`Hamster ${hamster.name} added to hamsters collection`);
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
+})();
+
+module.exports = addHamstersToDatabase;
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const addHamstersToDatabase = require("./addHamstersToDatabase");
 
 const app = express();
 
-app.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
 app.use(express.json());
 
 const hamstersRoute = require("./routes/hamsters");
 app.use("/hamsters", hamstersRoute);
 
-app.listen(3000, () => console.log("Server up and running at port 3000"));
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
