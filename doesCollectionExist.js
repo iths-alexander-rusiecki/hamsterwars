a5795bdb3db9f5fe7c8ad11b5040eb0438cf736e
Alexander Rusiecki <alexander.rusiecki@gmail.com>
2020-05-13 18:47:49 +0200

    .gitignore public/js

--- doesCollectionExist.js
+++ doesCollectionExist.js
@@ -1,8 +1,9 @@
 const { db } = require("./firebase");
 const addHamstersToDatabase = require("./addHamstersToDatabase");
 
-const doesCollectionExist = (() => {
-  db.collection("hamsters")
+const doesCollectionExist = (async () => {
+  await db
+    .collection("hamsters")
     .get()
     .then((query) => {
       if (query.size > 0) {
